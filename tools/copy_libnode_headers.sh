4608d3c670cf37b383dbe631de4992cf23da46e0
Yann <y.baelli05@gmail.com>
2026-04-09 10:50:18 +0200

    build: restore filtered mobile header packaging

--- tools/copy_libnode_headers.sh
+++ tools/copy_libnode_headers.sh
@@ -24,6 +24,39 @@ cd ..
 HEADERS=$OUT_DIR/libnode/include/node
 mkdir -p $HEADERS
 
+# Gyp
+cp common.gypi $HEADERS/common.gypi
+cp config.gypi $HEADERS/config.gypi
+
+# openssl headers
+mkdir -p $HEADERS/openssl/archs
+cp deps/openssl/openssl/include/openssl/*.h $HEADERS/openssl/
+if [ $1 == "ios" ]; then
+  rsync -am --include='*.h' -f 'hide,! */' \
+    -f '- aix64*' \
+    -f '- BSD*' \
+    -f '- darwin-i386*' \
+    -f '- linux*' \
+    -f '- solaris*' \
+    -f '- VC-WIN*' \
+    deps/openssl/config/* $HEADERS/openssl
+elif [ $1 == "android" ]; then
+  rsync -am --include='*.h' -f 'hide,! */' \
+    -f '- aix64*' \
+    -f '- BSD*' \
+    -f '- darwin*' \
+    -f '- linux-armv4*' \
+    -f '- linux-ppc*' \
+    -f '- linux32-s390x*' \
+    -f '- linux64-loongarch*' \
+    -f '- linux64-mips64*' \
+    -f '- linux64-riscv64*' \
+    -f '- linux64-s390x*' \
+    -f '- solaris*' \
+    -f '- VC-WIN*' \
+    deps/openssl/config/* $HEADERS/openssl
+fi
+
 # node headers
 cp src/js_native_api.h $HEADERS/
 cp src/js_native_api_types.h $HEADERS/
@@ -45,3 +78,7 @@ mkdir -p $HEADERS/cppgc
 cp deps/v8/include/cppgc/*.h $HEADERS/cppgc/
 mkdir -p $HEADERS/cppgc/internal
 cp deps/v8/include/cppgc/internal/*.h $HEADERS/cppgc/internal/
+
+# zlib headers
+cp deps/zlib/zconf.h $HEADERS/zconf.h
+cp deps/zlib/zlib.h $HEADERS/zlib.h
